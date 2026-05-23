670eff5d04d256ec3b256d77950c0c071fa0818c
Brett Bryan Sachinda <sachindabrett@gmail.com>
2026-05-23 18:40:16 +0200

    Configure Django for Render deployment

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1', '.onrender.com').split(',')
 
 # ================================================================
 # APPLICATIONS
@@ -147,3 +147,4 @@ EMAIL_SUBJECT_PREFIX = '[Wagwan Family] '
 # DEFAULT PRIMARY KEY
 # ================================================================
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
